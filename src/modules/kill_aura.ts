1e3fa2d8bf971e39e9b99bcd993bd205a22d7d38
Khao <khao@khaodoes.dev>
2025-10-27 19:30:34 +0700

    fix: improve killAura logic to correctly handle entity types and distances

--- src/modules/kill_aura.ts
+++ src/modules/kill_aura.ts
@@ -3,10 +3,12 @@ import type { Entity } from "prismarine-entity";
 import { config } from "../../config";
 
 export function killAura(bot: Bot) {
-  bot.on("entityMoved", (e: Entity) => {
-    if (config.modules.killAura.whitelist?.includes(e.type)) return;
-    if (e.position.distanceTo(bot.entity.position) <= 3) return;
+  bot.on("entityMoved", async (e: Entity) => {
+    if (config.modules.killAura.whitelist?.includes(e.name!)) return;
+    if (e.type !== "hostile") return;
+    if (e.position.distanceTo(bot.entity.position) > 3) return;
     // todo: get best weapon
+    await bot.lookAt(e.position.offset(0, e.height, 0), true); 
     bot.attack(e);
   });
 }
